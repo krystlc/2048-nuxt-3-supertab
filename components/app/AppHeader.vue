dd17bf833422fcbefc872954cc07a14ec5510bbf
krystlc <alex.valle@gmail.com>
2024-04-04 00:28:23 -0400

    feat: added menu hamburger

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,5 +1,34 @@
 <template>
-  <header class="text-center space-y-4 px-2 py-2 sm:py-8">
-    <a href="/"><AppBI /></a>
+  <header
+    class="flex items-center gap-4 p-4 sticky top-0 left-0 bg-black z-20 border-b border-orange-900"
+  >
+    <ClientOnly>
+      <button @click="showNav = true" class="sm:hidden">
+        <img src="@/assets/svgs/menu.svg" class="h-8 w-8 invert" />
+      </button>
+    </ClientOnly>
+    <RouterLink to="/" class="text-white hidden sm:inline-block">
+      <AppBI />
+    </RouterLink>
+    <h1 class="font-display text-2xl">2048: Cyber Fusion Edition</h1>
+    <AppMobileNav v-model="showNav" />
   </header>
 </template>
+
+<script setup lang="ts">
+const showNav = ref(false);
+
+const router = useRouter();
+router.beforeEach(() => {
+  showNav.value = false;
+});
+</script>
+
+<style scoped>
+nav ul li a {
+  @apply py-2 px-4 -mx-4 rounded-xl block hover:bg-slate-200;
+}
+nav ul li a.router-link-active {
+  @apply bg-slate-100;
+}
+</style>
