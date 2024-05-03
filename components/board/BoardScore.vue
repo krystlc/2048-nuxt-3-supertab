ccd8daa8cba60baa9ef1e3f835d09f0afb56dd6b
krystlc <alex.valle@gmail.com>
2024-05-02 23:21:37 -0400

    feat: added current attempt

--- components/board/BoardScore.vue
+++ components/board/BoardScore.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="board-score-wrapper">
     <div class="board-score">
+      <h5>Attempts</h5>
+      <p>{{ attempts }}</p>
+    </div>
+    <div class="board-score bg-orange-900">
       <h5>Score</h5>
       <p>{{ score }}</p>
     </div>
@@ -12,12 +16,12 @@
 </template>
 
 <script setup lang="ts">
-defineProps(["score", "highScore"]);
+defineProps(["score", "highScore", "attempts"]);
 </script>
 
 <style scoped>
 .board-score-wrapper {
-  @apply grid grid-cols-2 gap-2 mx-6 mb-4 bg-orange-950 p-2 rounded;
+  @apply grid grid-cols-3 gap-2 mb-4 bg-orange-950 p-2 rounded;
 }
 .board-score {
   @apply py-2 px-4 rounded;
