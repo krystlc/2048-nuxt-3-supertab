fb46e91c4c73c6bf6804e439dc8104c783209d00
krystlc <alex.valle@gmail.com>
2024-05-06 01:53:10 -0400

    feat: adjusted style of "tired of ad" cta

--- components/ads/AdsSquare.vue
+++ components/ads/AdsSquare.vue
@@ -1,43 +1,49 @@
 <template>
   <div
-    class="fixed h-screen inset-0 flex flex-col items-center justify-center gap-4"
+    class="fixed h-screen inset-0 flex items-center justify-center bg-black/90 backdrop-blur z-20"
   >
-    <div class="border shadow-2xl relative">
-      <a
-        class="block max-w-xs"
-        href="https://www.codewars.com/dashboard"
-        target="_blank"
-      >
-        <div class="absolute right-4 top-2 text-xs">
-          <p v-if="count >= 0">Ads ends in {{ count }}</p>
-        </div>
-        <img src="@/assets/gifs/ad2.gif" alt="Play Codewars" />
-        <div class="absolute inset-x-0 bottom-0 p-4">
-          <p class="text-2xl bg-black/90 border-b">Codewars</p>
-          <p class="text-xs">Solve puzzles by filling out the blanks!</p>
-        </div>
-      </a>
-      <button
-        v-if="count < 0"
-        @click="$emit('close')"
-        class="bg-black absolute right-2 top-2"
-      >
-        Close
-      </button>
-    </div>
-    <div class="relative z-20">
-      <button
-        class="bg-yellow-300 text-black py-2 px-6 w-full flex justify-between items-center hover:bg-black hover:text-yellow-300"
-        @click="handlePaywall"
-      >
-        <div class="text-left leading-none">
-          <div class="text-xs">Tired of the ads?</div>
-          <div class="uppercase tracking-widest font-semibold">
-            Play ad free!
+    <div class="max-w-xs space-y-4">
+      <div class="border shadow-2xl relative">
+        <a
+          class="block"
+          href="https://www.codewars.com/dashboard"
+          target="_blank"
+        >
+          <div class="absolute right-4 top-2 text-xs">
+            <p v-if="count >= 0">Ads ends in {{ count }}</p>
           </div>
-        </div>
-        <span class="text-xl">→</span>
-      </button>
+          <img src="@/assets/gifs/ad2.gif" alt="Play Codewars" />
+          <div class="absolute inset-x-0 bottom-0 p-4">
+            <p class="text-2xl bg-black/90 border-b">Codewars</p>
+            <p class="text-xs">Solve puzzles by filling out the blanks!</p>
+          </div>
+        </a>
+        <button
+          v-if="count < 0"
+          @click="$emit('close')"
+          class="bg-black absolute right-2 top-2"
+        >
+          Close
+        </button>
+      </div>
+      <div class="relative z-20">
+        <button
+          class="group relative inline-block font-medium text-red-600 focus:outline-none focus:ring active:text-red-500"
+          @click="handlePaywall"
+        >
+          <span class="absolute inset-0 border border-current"></span>
+          <span
+            class="block border border-current bg-white py-6 px-12 space-y-1 transition-transform group-hover:-translate-x-1 group-hover:-translate-y-1"
+          >
+            <span class="text-sm"
+              >Tired of the ads? <span class="text-xl">🙈</span></span
+            >
+            <span class="block text-lg leading-tight"
+              >Turn them <span class="font-black">off</span> for just 10¢</span
+            >
+          </span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
